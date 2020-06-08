f72aedf79140f24230fb7d6ecd844496573fd5bc
Niel Reitmann <nielreitmann@gmail.com>
2020-06-08 17:22:02 +1000

    first change

---
+++ home.js
@@ -0,0 +1,19 @@
+// var para = document.querySelectorAll(".explain p");
+// Array.from(para).forEach(function(para){
+//
+//   para.textContent += "(niel niel niel)";
+//
+//
+//
+// })
+
+// para.innerHTML += "<p>is this working?</p>";
+
+// console.log(para);
+//
+// document.getElementsByClassName('explain').innerHTML = "This is the new text";
+
+
+document.getElementById("p1").innerHTML = "New text!";
+
+console.log("this is a test");
